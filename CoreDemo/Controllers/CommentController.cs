08befa5c1a224c2e467bcee329353383ee2a6bd6
Yusuf Sabri Turhan <42yusuf05@gmail.com>
2021-10-10 02:02:01 +0300

    #30

---
+++ CoreDemo/Controllers/CommentController.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class CommentController : Controller
+    {
+
+        CommentManager com = new CommentManager(new EfCommentRepository());
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult CommentListByBlog(int id)
+        {
+            var values = com.GetList(id);
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialAddComment()
+        {
+            return PartialView();
+        }
+    }
+}
